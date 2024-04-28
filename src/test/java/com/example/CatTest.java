fd02c6d6ea2f4a7669b8b1ba107a934d3242233a
chunkychunk2 <chunoff@list.ru>
2024-04-28 21:09:32 +0300

    Убрал мок в тестируемом объекте

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,12 +2,9 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
     @Test
